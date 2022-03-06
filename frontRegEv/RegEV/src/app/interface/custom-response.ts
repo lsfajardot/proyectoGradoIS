2e090fa76fd28bfb11ba6780f273af13c833bb65
lsfajardot <luigifajardotoloza@gmail.com>
2022-03-05 22:34:16 -0500

    se inicia version nueva del frontend en angular

---
+++ frontRegEv/RegEV/src/app/interface/custom-response.ts
@@ -0,0 +1,11 @@
+import { Server } from "./servers";
+
+export interface CustomResponse {
+  timeStamp: Date;
+  statusCode: number;
+  status: string;
+  reason: string;
+  message: string;
+  developerMessage: string;
+  data: { servers?: Server[], server?: Server };
+}
